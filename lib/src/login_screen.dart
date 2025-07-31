734d1938a395650619b0bb197cb68fc9168015e3
ArielCatucuamba <davidcatudiaz22@gmail.com>
2025-07-30 20:51:53 -0500

    Update login_screen.dart

--- lib/src/login_screen.dart
+++ lib/src/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+//Ultima version
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
